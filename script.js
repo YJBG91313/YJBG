129be92a2e4add242a939922a0e413d216ca8db0
YJBG91313 <s1133827@g.ncyu.edu.tw>
2024-12-20 16:01:49 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ function showImg(){
 function show() {
  setInterval(showImg, 2000); //每隔2秒呼叫一次showImg函數
 }
+let tittle=document.getElementById("tittle");
+let content=document.getElementById("content");
+let btn=document.getElementById("btn");
+let list=document.getElementById("list");
+btn.addEventListener("click",news);
+function news(){
+  list.innerHTML=list.innerHTML + `
+  <div class="news">
+    <h2>${title.value}</h2>
+    <p>${content.value}</p><hr>
+  </div>
+  `;
+  title.value=""
+  content.value=""
+}
