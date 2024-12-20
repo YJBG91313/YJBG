ee9e2bbd2b09a4f74e555bb36d1358553d53d413
YJBG91313 <s1133827@g.ncyu.edu.tw>
2024-12-20 15:50:51 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let i=0, imgArr=new Array(); //宣告使用變數 i 及 陣列 imgArr
+imgArr[0] = "https://megapx-assets.dcard.tw/images/95e64206-994a-4e0f-bc3b-fc5848af3a1c/640.webp";
+imgArr[1] = "https://blog.delivered.co.kr/wp-content/uploads/2024/04/TXT-members-featured.jpg";
+imgArr[2] = "https://cdn2.ettoday.net/images/3914/d3914661.jpg";
+
+function showImg(){
+ document.getElementById("ico").src= imgArr[i];//指定img的src
+ i=(i+1) %3;
+}
+
+function show() {
+ setInterval(showImg, 2000); //每隔2秒呼叫一次showImg函數
+}
